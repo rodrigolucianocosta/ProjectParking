f8013eb7f1e126657716fa4c94bdd45abb7e456b
Rodrigo Luciano da Costa <rodrigolucianocosta@hotmail.com>
2015-04-17 19:22:56 -0300

    alteracoes na base e cracao do admim

--- ProjectParking/Parking/admin.py
+++ ProjectParking/Parking/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from models import vaga,carro
 # Register your models here.
+
+class vagaAdmin(admin.ModelAdmin):
+	list_display = ['numero','bloco']
+
+
+class carroAdmin(admin.ModelAdmin):
+	list_display = ['fabricante']
+
+admin.site.register(vaga,vagaAdmin)
+admin.site.register(carro,carroAdmin)
--- ProjectParking/Parking/models.py
+++ ProjectParking/Parking/models.py
@@ -1,3 +1,15 @@
+#unicode: utf-8
 from django.db import models
 
 # Create your models here.
+class vaga(models.Model):
+	numero = models.IntegerField('numero',max_length=2)
+	bloco  = models.CharField('bloco',max_length=2)
+
+	def __unicode__(self):
+		return u"%d %s"(self.numero,self.bloco)
+
+class carro(models.Model):
+	fabricante = models.CharField('fabricante',max_length=10)
+	def __unicode__(self):
+		return self.fabricante
