8caaa09509409839e2da9e9ec512cadf5dc2b2f6
Rodrigo Luciano da Costa <rodrigolucianocosta@hotmail.com>
2015-04-18 21:39:29 -0300

    tentativa de criar bootstrap

--- ProjectParking/Parking/views.py
+++ ProjectParking/Parking/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 def index(request):
         context = {'texto': 'Projeto Django no Linux/Ubuntu com Sublime Text 3 avaliação 2'}
-        return render(request, 'index.html', context)
+        
+        return render(request, 'Parking/index.html', context)
